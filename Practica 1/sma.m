4cf78fe53604c7a43b42e0ef95267da9f33e2404
tardisfromtornspace <andromedavirgoa@gmail.com>
2023-03-09 16:58:50 +0100

    Practice 3 up to point 2a

--- Practica 1/sma.m
+++ Practica 1/sma.m
@@ -9,7 +9,7 @@ function [y,m] = sma(x,n,M,ac) % x is the input sequence and M+1 is the number o
     y = zeros(1, length(x));
     aux = [ac x];
     if M > length(ac)
-        M = length(ac)
+        M = length(ac);
     end
     for a=1:length(x)
         y(a) = sum(aux(a-M+length(ac):a+length(ac)))/(M+1);
---
+++ Practica 1/z_trans_finite.m
@@ -0,0 +1,7 @@
+function [X] = z_trans_finite(x,n) % x is the input sequence and n is the range of the discrete independent variable
+    syms z
+    X(z) = sum(x.* z.^(-n))
+end
+
+
+
