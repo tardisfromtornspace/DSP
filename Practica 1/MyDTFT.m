509c2f802770aec5cbab0019613c56ef368e69f0
tardisfromtornspace <andromedavirgoa@gmail.com>
2023-04-27 17:06:30 +0200

    Practice 5 finished

---
+++ Practica 1/MyDTFT.m
@@ -0,0 +1,20 @@
+function [F] = MyDTFT(x,n)
+    syms w
+
+    ni = n(1);
+    nf = n(end); 
+
+    %axis([ni,nf,0,2])
+
+    F(w)= sum(x.*exp(-1i*w*n));
+    %F(w)=simplify(F(w));
+    disp('DT Fourier Transform: ')
+    F(w)
+    subplot(2,1,1); fplot((real(F).^2 + imag(F).^2).^(1/2),[-pi,pi]) % no reconoce las abs(F) para este caso, de forma extraña, probamos la definición
+    subplot(2,1,2); fplot(phase(F),[-pi,pi]) %angle(F)
+
+end
+
+
+
+
---
+++ Practica 1/MyiDTFT.m
@@ -0,0 +1,17 @@
+function [x, n] = MyiDTFT(F,N)
+    if N < 0
+        N = -N;
+    end
+    n = -N:N;
+
+    x = int(F *exp(1i*n*symvar(F)),-pi,pi)/(2*pi);
+
+    disp('DT Fourier Inverse Transform: ')
+    x
+    stem(n, x);
+
+end
+
+
+
+
